4abcd8a0495bb6c97479f2c5a3eb3c3f385c917a
Laureano <laureanosciacaluga@gmail.com>
2023-06-17 11:55:02 -0300

    Etapa de maquetacion

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+</head>
+<body>
+    <div style="height:100vh; border:1px solid">
+
+        <div class="" style="height:15%; border:1px solid">
+            
+        </div>
+    
+        <div class="row" style="height:70%; border:1px solid">
+            <div class="col-9" style="height:100%; border:1px solid"></div>
+            <div class="col-3" style="height:100%; border:1px solid"></div>
+        </div>
+        
+        <div class="" style="height:15%; border:1px solid">
+        
+        </div>
+    </div>
+
+
+
+
+
+
+    <!-- BOOTSTRAP -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+</body>
+</html>
