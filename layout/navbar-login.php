ab33fb40092e07495bcf82d280e585487c56ce84
Laureano <laureanosciacaluga@gmail.com>
2023-06-19 15:18:15 -0300

    login visual v1

---
+++ layout/navbar-login.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-lg navegador w-100">
+  <div class="container-fluid h-100 p-0 ms-3 me-3">
+    <a class="navbar-brand" href="http://eetp669.edu.ar/">
+        <img src="images/logo-escuela.jpg" alt="logo" width="50px">
+        Pagina escuela
+    </a>
+  </div>
+</nav>
---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- styles -->
+    <link rel="stylesheet" href="styles/stylesLogin.css">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- BOOTSTRAP ICONS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+</head>
+<body>
+    <div style="height:100vh; border:1px solid" class="divPadre">
+        <?php
+            include('layout/navbar-login.php');
+        ?>  
+
+        <div class="container containerAcceso">
+            <form action="" class="formularioLogin">
+                <div class="row">
+                    <h2>Acceso</h2>
+                </div>
+                <div class="row">
+                    <div class="input-group mb-3 mt-4">
+                        <span class="input-group-text" id="basic-addon1"><i class="bi bi-person"></i></span>
+                        <input type="text" class="form-control" placeholder="Usuario" name="Usuario" aria-describedby="basic-addon1" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1"><i class="bi bi-key"></i></span>
+                        <input type="password" class="form-control" placeholder="Contraseña" name="Contraseña" aria-describedby="basic-addon1" required>
+                    </div>
+                </div>
+                <div class="row">
+                    <input class="btn btn-primary" type="submit" value="Acceder">
+                </div>
+            </form>
+        </div>
+
+
+
+    </div>
+</body>
+</html>
